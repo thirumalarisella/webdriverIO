44ca26fd7b1bd648193b7b20a45fde1fc5e3d9da
Thirumalesh <arisellathirumalesh@gmail.com>
2023-04-03 12:04:21 +0530

    headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,11 +59,16 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+        'goog:chromeOptions': {
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            args: ['--headless', '--disable-gpu'],
+            }
     }],
     //
     // ===================
